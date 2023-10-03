e5887cd471956b38c1e25a913c03356076a3d469
Samanta Goulong <samanta1004@hotmail.com>
2023-10-03 16:10:29 +0200

    feat: changement de l'image au clic

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -16,30 +16,53 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+let photo = 0
+let text = 0
+// console.log(slides[photo].image);
 
 const flècheGauche = document.querySelector(".arrow_left")
-		console.log (flècheGauche)
+		// console.log (flècheGauche)
 flècheGauche.addEventListener("click", function () {
-		console.log("vous avez cliqué sur le bouton gauche");
-	
+		//console.log("vous avez cliqué sur le bouton gauche");
+	photo--
+	let picture = document.querySelector(".banner-img");
+	picture.src = "./assets/images/slideshow/" + slides[photo].image;
+	//console.log(photo);
+
+	text--
+	let title = document.querySelector("p");
+	title.innerHTML = ""+ slides[text].tagLine ;
+	console.log(title);
 });
 
 
 const flècheDroite = document.querySelector(".arrow_right")
-			console.log(flècheDroite);
+			//console.log(flècheDroite);
 flècheDroite.addEventListener("click", function () {
-			console.log("Vous avez cliqué sur le bouton droit")
-	
+			//console.log("Vous avez cliqué sur le bouton droit")
+	photo++
+	let picture = document.querySelector(".banner-img");
+	picture.src = "./assets/images/slideshow/" + slides[photo].image;
+	//console.log(photo);
+
+	text++
+	let title = document.querySelector("p");
+	title.innerHTML = ""+ slides[text].tagLine ;
+	console.log(title);
 	});
 
+
+
 for (let compteur = 0; compteur < slides.length; compteur++){
-		console.log(slides[compteur]);
+		//console.log(slides[compteur]);
 let dot = document.createElement("div");
 let bullets = document.querySelector(".dots");
 bullets.appendChild(dot)
-		console.log(bullets)
+		//console.log(bullets)
 dot.classList.add("dot")
 }
 
+let dotSelected = document.querySelector(".dot")
+dotSelected.classList.add("dot_selected")
 
 
